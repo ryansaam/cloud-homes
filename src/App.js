840c3c5bf8cde7b36995c58d267b2a51d6534fe0
Ryan Sam <ryansam.inquiries@gmail.com>
2019-01-28 06:40:17 -0600

    Added home screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Home from './components/Home.js'
+
+// fetch()
+//   .then(function(response) {
+//     return response;
+// })
+// .then(function(myJson) {
+//   console.log(myJson);
+// });
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Cloud Homes</h1>
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import img1 from '../images/home.jpeg'
+const Home = props => {
+  return (
+    <div className="home" style={{backgroundImage: `url(${img1})`}} >
+      
+    </div>
+  )
+}
+
+export default Home
